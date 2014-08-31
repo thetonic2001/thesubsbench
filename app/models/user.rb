97ab2dc5abe071922e8cd55ae55122cb68b35a9b
Tony McKeown <thetonic2001@gmail.com>
2014-08-31 04:39:09 +0100

    Created method to display date of birth as an age on profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,17 @@ validates :profile_name, presence: true,
 						 }
 
         
-        def to_param
-	profile_name
-end
-                   
+  def to_param
+		profile_name
+	end
+
   def full_name
 		first_name + " " + last_name
 	end
+
+	def age
+	  now = Time.now.utc.to_date
+	  now.year - date_of_birth.year - ((now.month > date_of_birth.month || (now.month == date_of_birth.month && now.day >= date_of_birth.day)) ? 0 : 1)
+	end
+
 end
